4b184bead06d93dec569dea5ec09ae945e6f26f9
eitaneitan7 <eitaneitan7@gmail.com>
2024-01-22 13:33:25 +0200

    Semi completed the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL
+    images:{
+        domains:['lh3.googleusercontent.com']
+    }
 }
 
 module.exports = nextConfig
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs:ClassValue[]){
     return twMerge(clsx(inputs))
 }
+
+export function chatHrefConstructor(id1:string,id2:string){
+    const sortedId = [id1,id2].sort()
+    return `${sortedId[0]}--${sortedId[1]}`
+}
+
+export function toPusherKey(key:string){
+    return key.replace(/:/g,'__')
+}
